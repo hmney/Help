a719f27e5d5fb3a00a8da7e5a6ad994dd033b983
Houssam-Eddine Mney <39967886+hmney@users.noreply.github.com>
2020-09-01 19:28:50 +0100

    Update rev_arr.c
    check this code bro

--- rev_arr.c
+++ rev_arr.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 
-void	ft_sort(int *tab, int size)
+void ft_sort(int *tab, int size)
 {
-	int i;
-	int rev;
-	i = 0;	
-		while(i < size)
-		{	
-			if(tab[i] > tab[i - 1])
-			{
-				rev = tab[i];
-				tab[i] = tab[i - 1];
-				tab[i - 1] = rev;
-				i = 0;
-			}
-			else
-				i++;
-		}
-	}
+    int i;
+    int rev;
+    i = 1;
+    while (i < size)
+    {
+        if (tab[i] > tab[i - 1])
+        {
+            rev = tab[i];
+            tab[i] = tab[i - 1];
+            tab[i - 1] = rev;
+            i = 1;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
 
-int	main()
+int main()
 {
-	int i;
-	int n;
-	int tab[] = {1, 5, 7, 99};
-	n = sizeof(tab) / sizeof(tab[0]);
-	ft_sort(tab, n);
-	for(i = 0; i < n; i++)
-		printf("%d\n", tab[i]);
-	return(0);}
-
+    int i;
+    int n;
+    int tab[] = {1, 5, 7, 99, 8 , 0};
+    n = sizeof(tab) / sizeof(tab[0]);
+    ft_sort(tab, n);
+    for (i = 0; i < n; i++)
+        printf("%d\n", tab[i]);
+    return (0);
+}
