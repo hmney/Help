dcfd88299e249868331f558402da6b49e6068b8f
rmney <riyadati.rm@gmail.com>
2020-09-01 19:19:19 +0100

    ec

---
+++ rev_arr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void	ft_sort(int *tab, int size)
+{
+	int i;
+	int rev;
+	i = 0;	
+		while(i < size)
+		{	
+			if(tab[i] > tab[i - 1])
+			{
+				rev = tab[i];
+				tab[i] = tab[i - 1];
+				tab[i - 1] = rev;
+				i = 0;
+			}
+			else
+				i++;
+		}
+	}
+
+int	main()
+{
+	int i;
+	int n;
+	int tab[] = {1, 5, 7, 99};
+	n = sizeof(tab) / sizeof(tab[0]);
+	ft_sort(tab, n);
+	for(i = 0; i < n; i++)
+		printf("%d\n", tab[i]);
+	return(0);}
+
